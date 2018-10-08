721767c269cbffaf862d800b838d627a85017077
Jamie Weldon <jamie.weldon@coyote.com>
2018-10-08 20:05:31 +0000

    Merged PR 4776: update Copy-Item in predeploy to remove credential
    update Copy-Item in predeploy to remove credential

--- predeploy.ps1
+++ predeploy.ps1
@@ -4,7 +4,7 @@
 #$file = "\\gxstorage\GXData\All Departmental Data\Information Technology\Jamie\Miniconda3-latest-Windows-x86_64.exe"
 $file = "\\gxstorage\GXData\Test Files\Miniconda3-latest-Windows-x86_64.exe"
 
-Copy-Item -Credential -Path $file -Destination 'c:\temp\Miniconda3-latest-Windows-x86_64.exe'
+Copy-Item -Path $file -Destination 'c:\temp\Miniconda3-latest-Windows-x86_64.exe'
 
 Invoke-Command -ScriptBlock {
     Start-Process "c:\temp\Miniconda3-latest-Windows-x86_64.exe" -ArgumentList "/S", "/InstallationType=AllUsers", "/RegisterPython=1", "/D=C:\Miniconda" -Wait
