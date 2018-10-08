e39ffc03897876aedb089d6c9fce76bb11187319
Jamie Weldon <jamie.weldon@coyote.com>
2018-10-08 19:49:49 +0000

    Merged PR 4775: update $file to share w/ rights
    update $file to share w/ rights

--- predeploy.ps1
+++ predeploy.ps1
@@ -1,9 +1,10 @@
 ###########################################################################################################################
 #Copy and install Miniconda
 ###########################################################################################################################
-$file = "\\gxstorage\GXData\All Departmental Data\Information Technology\Jamie\Miniconda3-latest-Windows-x86_64.exe"
+#$file = "\\gxstorage\GXData\All Departmental Data\Information Technology\Jamie\Miniconda3-latest-Windows-x86_64.exe"
+$file = "\\gxstorage\GXData\Test Files\Miniconda3-latest-Windows-x86_64.exe"
 
-Copy-Item -Path $file -Destination 'c:\temp\Miniconda3-latest-Windows-x86_64.exe'
+Copy-Item -Credential -Path $file -Destination 'c:\temp\Miniconda3-latest-Windows-x86_64.exe'
 
 Invoke-Command -ScriptBlock {
     Start-Process "c:\temp\Miniconda3-latest-Windows-x86_64.exe" -ArgumentList "/S", "/InstallationType=AllUsers", "/RegisterPython=1", "/D=C:\Miniconda" -Wait
