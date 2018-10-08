079916d60ea5a19dd716cd8f7a0a7dcb1cab2ef6
Jamie Weldon <jamie.weldon@coyote.com>
2018-10-08 19:32:46 +0000

    Merged PR 4773: change predeploy script to run from deployed machine
    change predeploy script to run from deployed machine

--- predeploy.ps1
+++ predeploy.ps1
@@ -1,12 +1,10 @@
-$session = New-PSSession -ComputerName PRDGXWEBSRV01 #-Credential $cred
-$file = "c:\installers\Miniconda3-latest-Windows-x86_64.exe"
+###########################################################################################################################
+#Copy and install Miniconda
+###########################################################################################################################
+$file = "\\gxstorage\GXData\All Departmental Data\Information Technology\Jamie\Miniconda3-latest-Windows-x86_64.exe"
 
-#Copy installer executable
-Copy-Item -Path $file -ToSession $session -Destination 'c:\temp\Miniconda3-latest-Windows-x86_64.exe'
+Copy-Item -Path $file -Destination 'c:\temp\Miniconda3-latest-Windows-x86_64.exe'
 
-#Install miniconda
-Invoke-Command -Session $session -ScriptBlock {
+Invoke-Command -ScriptBlock {
     Start-Process "c:\temp\Miniconda3-latest-Windows-x86_64.exe" -ArgumentList "/S", "/InstallationType=AllUsers", "/RegisterPython=1", "/D=C:\Miniconda" -Wait
 }
-
-Remove-PSSession $session
