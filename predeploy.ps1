56a451bf7f9676f2244a5709e6b484541341b0c5
Jamie Weldon <jamie.weldon@coyote.com>
2018-10-08 20:34:45 +0000

    Merged PR 4779: adding write-host to predeploy
    adding write-host to predeploy

--- predeploy.ps1
+++ predeploy.ps1
@@ -1,7 +1,7 @@
 ###########################################################################################################################
 #Download and install Miniconda
 ###########################################################################################################################
-
+Write-Host "predeploy.ps1 process - downloading miniconda"
 $url = "https://repo.continuum.io/miniconda/Miniconda3-latest-Windows-x86_64.exe"
 $output = "c:\temp\Miniconda3-latest-Windows-x86_64.exe"
 $start_time = Get-Date
@@ -9,6 +9,8 @@ $start_time = Get-Date
 $wc = New-Object System.Net.WebClient
 $wc.DownloadFile($url, $output)
 
+
+Write-Host "predeploy.ps1 process - installing miniconda"
 Invoke-Command -ScriptBlock {
     Start-Process "c:\temp\Miniconda3-latest-Windows-x86_64.exe" -ArgumentList "/S", "/InstallationType=AllUsers", "/RegisterPython=1", "/D=C:\Miniconda" -Wait
 }
